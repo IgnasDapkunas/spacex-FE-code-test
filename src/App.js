cf2f24032cc81f2e8f8a06cfb245ef6deb5e852b
IgnasDapkunas <ignas.dapk@gmail.com>
2021-03-24 15:42:11 +0200

    project completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LaunchPage from "./LaunchPage";
+import HomePage from "./HomePage";
 
-function App() {
+export default function App() {
+  const [loading, setLoading] = useState(true);
+  const [flightKey, setFlightKey] = useState();
+  const [data, setData] = useState([]);
+
+  const baseUrl = "https://api.spacexdata.com/v2";
+  const launchesData = async () => {
+    const response = await fetch(`${baseUrl}/launches`);
+    setData(await response.json());
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    launchesData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <HomePage prop={{ loading, data }} onClick={(number) => setFlightKey(number)} />
+        </Route>
+        <Route path="/launchpage">
+          <LaunchPage prop={{ data }} flightKey={flightKey} />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
-
-export default App;
